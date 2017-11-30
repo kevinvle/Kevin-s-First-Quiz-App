9a585ba30dd847547ab80694e7ad0d40588ac0cf
Kevin Viet Le <kevin1le@hotmail.com>
2017-11-30 10:47:12 -0800

    First

---
+++ Kevin's First Quiz Game/Controller/MenuViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Kevin's First Quiz Game
+//
+//  Created by Kevin Le on 11/30/17.
+//  Copyright © 2017 Kevinvle. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    private let contentView = UIView()
+    private let logoView = UIImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
---
+++ Kevin's First Quiz GameTests/Kevin_s_First_Quiz_GameTests.swift
@@ -0,0 +1,36 @@
+//
+//  Kevin_s_First_Quiz_GameTests.swift
+//  Kevin's First Quiz GameTests
+//
+//  Created by Kevin Le on 11/30/17.
+//  Copyright © 2017 Kevinvle. All rights reserved.
+//
+
+import XCTest
+@testable import Kevin_s_First_Quiz_Game
+
+class Kevin_s_First_Quiz_GameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
