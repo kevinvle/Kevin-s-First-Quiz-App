0729298203fec86fec59e0637d029f489a4d4df8
Kevin Viet Le <kevin1le@hotmail.com>
2017-12-09 23:03:33 -0800

    Update

--- Kevin's First Quiz Game/Controller/MenuViewController.swift
+++ Kevin's First Quiz Game/Controller/MenuViewController.swift
@@ -41,21 +41,21 @@ class MenuViewController: UIViewController {
     }
 
     func layoutView() { 
-        contentView.translateAutoresizingMaskIntoConstraints = false  // That is the first step we need for our anchor points when we do autolayoutencode
+        contentView.translatesAutoresizingMaskIntoConstraints = false  // That is the first step we need for our anchor points when we do autolayoutencode
         view.addSubview(contentView)
 
-        logoView.translateAutoresizingMaskIntoConstraints = false
+        logoView.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(logoView)
 
-        buttonView.translateAutoresizingMaskIntoConstraints = false
+        buttonView.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(buttonView)
 
         for (index, title) in titles.enumerated() { // For loop will give us current title and index
-            let button = Roundedbutton()
-            button.translateAutoresizingMaskIntoConstraints = false
+            let button = RoundedButton()
+            button.translatesAutoresizingMaskIntoConstraints = false
             buttonView.addSubview(button)
             button.backgroundColor = UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0)
-            button.titleLabel?.font = UI.Font.boldSystemFont(ofSize: 20)  // Set font of buttons
+            button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)  // Set font of buttons
             button.setTitle(title, for: .normal)  // Set current title
             button.tag = index  // something we'll need later on
             gameButtons.append(button)  
@@ -65,19 +65,19 @@ class MenuViewController: UIViewController {
 
         }
         
-        scoreView.translateAutoresizingMaskIntoConstraints = false
+        scoreView.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(scoreView)
 
-        titleLabel.translateAutoresizingMaskIntoConstraints = false
-        recentScoreLabel.translateAutoresizingMaskIntoConstraints = false // SHows recent score
-        highscoreLabel.translateAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        recentScoreLabel.translatesAutoresizingMaskIntoConstraints = false // SHows recent score
+        highscoreLabel.translatesAutoresizingMaskIntoConstraints = false
         scoreView.addSubview(titleLabel)
         scoreView.addSubview(recentScoreLabel)
         scoreView.addSubview(highscoreLabel)
 
         titleLabel.textAlignment = .center
         titleLabel.font = UIFont.boldSystemFont(ofSize: 30)
-        titleLabel.textColor = UI.Color.white
+        titleLabel.textColor = UIColor.white
         recentScoreLabel.font = UIFont.boldSystemFont(ofSize: 20)
         recentScoreLabel.textColor = UIColor.white
         highscoreLabel.font = UIFont.boldSystemFont(ofSize: 20)
@@ -86,10 +86,10 @@ class MenuViewController: UIViewController {
 
         titleLabel.text = "Multiple Choice"
         recentScoreLabel.text = "Recent: 0"
-        highscoreLabel.textColor = "Highscore: 0"
+        highscoreLabel.text = "Highscore: 0"
 
         let constraints = [
-            contentView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8.0), // top
+            contentView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8.0), // top also: """"'topLayoutGuide' was deprecated in iOS 11.0: Use view.safeAreaLayoutGuide.topAnchor instead of topLayoutGuide.bottomAnchor""""
             contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),  // left anchor
             contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),  // right anchor
             contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),   // bottom
@@ -117,7 +117,7 @@ class MenuViewController: UIViewController {
             gameButtons[3].trailingAnchor.constraint(equalTo: buttonView.trailingAnchor),
             gameButtons[0].heightAnchor.constraint(equalTo: gameButtons[1].heightAnchor), // These 3 lines make sure all our buttons are positioned correctly and are equal in size
             gameButtons[1].heightAnchor.constraint(equalTo: gameButtons[2].heightAnchor),
-            gameButtons[0].heightAnchor.constraint(equalTo: gameButtons[1].heightAnchor),
+            gameButtons[2].heightAnchor.constraint(equalTo: gameButtons[3].heightAnchor),
             scoreView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -40.0),
             scoreView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.6),
             scoreView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.3),
@@ -131,7 +131,7 @@ class MenuViewController: UIViewController {
             recentScoreLabel.bottomAnchor.constraint(equalTo: highscoreLabel.topAnchor, constant: -8.0),
             highscoreLabel.leadingAnchor.constraint(equalTo: scoreView.leadingAnchor),
             highscoreLabel.trailingAnchor.constraint(equalTo: scoreView.trailingAnchor),
-            highscoreLabel.bottomAnchor.constraint(equal: scoreView.bottomAnchor, constant: -8.0),
+            highscoreLabel.bottomAnchor.constraint(equalTo: scoreView.bottomAnchor, constant: -8.0),
             titleLabel.heightAnchor.constraint(equalTo: recentScoreLabel.heightAnchor),
             recentScoreLabel.heightAnchor.constraint(equalTo: highscoreLabel.heightAnchor)
 
